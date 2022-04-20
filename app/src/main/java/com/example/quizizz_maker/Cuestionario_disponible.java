e0b71b2ffb3b5ae0c581405eca1c132804bb6ca1
katterincampos <88002663+katterincampos@users.noreply.github.com>
2022-04-20 17:01:20 -0600

    interfaces completas

---
+++ app/src/main/java/com/example/quizizz_maker/Cuestionario_disponible.java
@@ -0,0 +1,14 @@
+package com.example.quizizz_maker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cuestionario_disponible extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuestionario_disponible);
+    }
+}
---
+++ app/src/main/java/com/example/quizizz_maker/Nota_final_estudiante.java
@@ -0,0 +1,14 @@
+package com.example.quizizz_maker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nota_final_estudiante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nota_final_estudiante);
+    }
+}
